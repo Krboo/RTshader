6e1cb48bd240056c375ac1c41cfda33e5d873e0e
antoine vially <Syberion@MacBook-Pro-de-antoine.local>
2017-04-12 15:39:47 +0200

    rotation

--- old.glsl
+++ old.glsl
@@ -33,38 +33,16 @@ struct	Coupe_S
 	vec3	rot;
 };
 
-/* Découpe de sphère */
-
-bool decoupe(vec3 centre, vec3 inter, Coupe_S c, Coupe_S c2)
+/* rotation d'un point auour des 3 axes */
+vec3 rotate(vec3 point, vec3 rot)
 {
-	vec3 pos = centre + (c.pos * normalize(c.rot));
-	vec3 pos2 = centre + (c2.pos * normalize(c2.rot));
+	rot = rot * M_PI / 180;
 
-	float d = (c.rot.x * pos.x + c.rot.y * pos.y + c.rot.z * pos.z) * -1;
-
-	if (c.rot.x * inter.x + c.rot.y * inter.y + c.rot.z * inter.z + d > 0)
-		return(true);
-
-	return(false);
+	mat3 rotation = mat3(0,-1 * cos(rot.y) * sin(rot.z),sin(rot.y),
+						sin(rot.z) * cos(rot.x),sin(rot.x) * sin(rot.y) * sin(rot.z) + cos(rot.x) * cos(rot.z),-1 * sin(rot.x) * cos(rot.y),
+						sin(rot.x) * sin(rot.z),cos(rot.x) * sin(rot.y) * sin(rot.z) + sin(rot.x) * cos(rot.z), cos(rot.x) * cos(rot.y));
+	return (point * rotation);
 }
-
-/* Intersection rayon / plan */
-
-/*void plane(vec3 norm, vec3 pos, vec3 color, vec4 data, Ray r, inout Hit h)
-{
-	float t = (dot(norm,pos) - (dot (norm, r.pos))) / dot (norm, r.dir);
-
-	if (t < EPSI)
-		return;
-
-	if (t < h.dist) {
-		h.dist = t;
-		h.pos = r.pos + r.dir * h.dist;
-		h.color = color;
-		h.norm = (faceforward (norm, norm, r.dir));
-		h.data = data;
-	}
-}*/
 
 /*
 vec4 atlas_fetch(vec4, vec2 obj_uv) {
@@ -73,9 +51,9 @@ vec4 atlas_fetch(vec4, vec2 obj_uv) {
 }
 */
 
-/* Intersection rayon / plan limité (obsolete on le fera en mesh) */
-
-void plane (vec3 norm, vec3 pos, vec3 rot, float data, Material mat, Ray r, inout Hit h) {
+/* Intersection rayon / plan limité */
+void plane (vec3 norm, vec3 pos, vec3 rot,float data, Material mat, Ray r, inout Hit h) {
+	norm = normalize(norm);
 	float t = (dot(norm,pos) - (dot (norm, r.pos))) / dot (norm, r.dir);
 	Hit tmp = h;
 
@@ -92,9 +70,15 @@ void plane (vec3 norm, vec3 pos, vec3 rot, float data, Material mat, Ray r, inou
 		return;
 	else if(h.pos.x > pos.x + data/2 || h.pos.x < pos.x - data/2  || h.pos.y > pos.y + data/2 || h.pos.y < pos.y - data/2 || h.pos.z > pos.z + data/2 || h.pos.z < pos.z - data/2)
 		h = tmp;
+	else{
+		h.pos = rotate(h.pos, rot);
+		h.norm = rotate(h.norm, rot);
+		vec3 tmp = (h.pos - r.pos) * (h.pos - r.pos);
+		h.dist = sqrt(tmp.x + tmp.y + tmp.z);
+	}
 }
 
-/* Intersection rayon / sphère */
+/* Intersection rayon sphère */
 
 void sphere (vec3 pos, float data, Material mat, Ray r, inout Hit h) {
 	vec3 d = r.pos - pos;
@@ -109,15 +93,8 @@ void sphere (vec3 pos, float data, Material mat, Ray r, inout Hit h) {
 		return;
 
 	float t = (-sqrt (g) - b) / a;
-	//Coupe_S coupe;
-	//coupe.pos = 0.5;
-	//coupe.rot = vec3(0,1,0);
 
-	//Coupe_S coupe2;
-	//coupe2.pos = 1.2;
-	//coupe2.rot = vec3(0,0,1);
-
-	if (t < 0)//|| decoupe(pos, h.pos, coupe, coupe2))
+	if (t < 0)
 		return;
 
 	if (t < h.dist) {
@@ -205,9 +182,6 @@ void cone(vec3 v, vec3 dir, float data, Material mat, Ray r, inout Hit h)
 /* Fonction du calcul de l'intersection entre un rayon et un cube (manque la rotation) */
 void cube(vec3 pos, vec3 rot, float data, Material mat, Ray r, inout Hit hit)
 {
-	r.dir -= r.pos;
-	r.pos += rot;
-	r.dir += r.pos;
 	plane(vec3(0, 0, 1),vec3(pos.x,pos.y,pos.z - data/2),rot, data, mat, r, hit);
 	plane(vec3(0, 0, 1),vec3(pos.x,pos.y,pos.z + data/2),rot, data, mat, r, hit);
 	plane(vec3(0, 1, 0),vec3(pos.x,pos.y - data/2,pos.z),rot, data, mat, r, hit);
@@ -222,9 +196,8 @@ Hit		scene(Ray r)
 	Hit		hit;
 	hit.dist = 1e20;
 	hit.mat.texture = vec4(0,0,0,0);
-  //size + reflexion + ? + ?
-	sphere(vec3(15, 5, -10), 5, Material(vec4(1,0,1,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(1,1,1,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,0,0,0)), r, hit );
-	sphere(vec3(8, 9, -30), 5, Material(vec4(0.8,0.8,0.5,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,1,1,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,0,0,0)), r, hit );
+	//sphere(vec3(15, 5, -10), 5, Material(vec4(1,0,1,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(1,1,1,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,0,0,0)), r, hit );
+	//sphere(vec3(8, 9, -30), 5, Material(vec4(0.8,0.8,0.5,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,1,1,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,0,0,0)), r, hit );
 	//sphere(vec3(8, 9, -30), vec3(0,0,1), vec4(5,1,1,0), r, hit);
 	//sphere(vec3(15, 15, -45), vec3(0.8,0.5,0), vec4(5,1,1,0), r, hit);
 	//sphere(vec3(20, 30, -50), vec3(0.75,0.5,0.55), vec4(4,1,1,0), r, hit);
@@ -232,10 +205,10 @@ Hit		scene(Ray r)
 	//cyl(vec3(0, 0, 0), vec3(1,0,0), vec3(1,0,0), vec4(1,0,1,0), r, hit);
 	//cyl(vec3(0, 0, 0), vec3(0,1,0), vec3(0,1,0), vec4(1,0,1,0), r, hit);
 	//cone(vec3(75, 10, 200), vec3(1,1,0), vec3(1,1,0), vec4(30,0,0,0), r, hit);
-	plane(vec3(1,1,0),vec3(1,1,-6), vec3(0,0,0), 0.0, Material(vec4(0.5,0.7,0.8,0), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,1,1,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,0,0,0)), r, hit);
+	plane(vec3(45,35,90),vec3(1,1,-6), vec3(0,0,0), 0.0, Material(vec4(0.5,0.7,0.8,0), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,1,1,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,0,0,0)), r, hit);
 	//plane(vec3(0,1,0),vec3(50,-280,-30),vec3(0,0,0),vec3(1,1,1),vec4(0,0,0,0), r, hit);
 	//plane(vec3(1,1,1),vec3(50,-280,-30),vec3(0,0,0),vec3(0.3,0.5,0.6),vec4(0,0,0,0), r, hit);
-	//cube(vec3(5, 15, -25), vec3(0,0,0), vec3(0.8,0.5,0.5), vec4(2,1,1,0), r, hit);
+	cube(vec3(5, 15, -25), vec3(45,45,0), 4.,Material(vec4(0.8,0.8,0.5,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,1,1,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,0,0,0)), r, hit);
 	//sphere(vec3(45, 20, 25),vec3(255,255,255),vec4(0.5, 0.6, 0.5, 0.2), r, hit);
 	return hit;
 }

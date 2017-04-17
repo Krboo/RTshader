3fe367da4818f0a4b5f5cd8d9d7b0c56cecec6bc
LaBoiteAGogo <Syberion@MacBook-Pro-de-antoine.local>
2017-04-17 15:11:58 +0200

    decoupe

--- yalaouf.glsl
+++ yalaouf.glsl
@@ -1,224 +1,257 @@
-#define EPSILONE	0.01f
-#define	M_PI		3.14159265359	
+#define M_PI 3.1415926535897932384626433832795
+#define EPSI 0.001f
+#define AMBIENT 0.2
 
-// Strcuture pour les rayons, la lumière, les collisions et les objets.
-struct	Ray
+struct		Ray
 {
 	vec3	dir;
 	vec3	pos;
 };
 
-struct	Light
+struct Material
 {
-	vec3	dir;
+	vec4	texture;
+	vec4	emission; //"a voir plus tard"
+	vec4	transparency;
+	vec4	reflection;
+	vec4	refraction;
+	vec4	bumpmap;
+	vec4	specular;
+};
+
+struct		Hit
+{
+	float	dist;
+	vec3	norm;
 	vec3	pos;
+	Material mat;
 };
 
-struct	Hit
+struct	Coupe
 {
-	float		dist;
-	vec3		norm;
-	vec3		pos;
-	vec3		color;
-	vec2		uv;
+	float	dist;
+	vec3	rot;
 };
 
-struct			Sphere
-{
-	vec3		pos;
-	vec3		color;
-	float		radius;
-};
+void plane (vec3 norm, vec3 pos, float data, Material mat, Ray r, inout Hit h) {
+	norm = normalize(norm);
+	float t = (dot(norm,pos) - (dot (norm, r.pos))) / dot (norm, r.dir);
+	Hit tmp = h;
+	vec3 plus = pos + data/2;
+	vec3 moin = pos - data/2;
 
-struct			Plane
-{
-	vec3		pos;
-	vec3		norm;
-	vec3		color;
-};
+	if (t < EPSI)
+		return;
 
-struct	Cylinder
-{
-	vec3	pos;
-	vec3	dir;
-	vec3	color;
-	float	radius;
-};
-
-struct	Cone
-{
-	vec3	pos;
-	vec3	dir;
-	vec3	color;
-	float	angle;
-};
-
-
-float	limit(float value, float minv, float maxv)
-{
-	return (value < minv ? minv : (value > maxv ? maxv : value));
-}
-
-void	iSphere(Sphere s, Ray r, inout Hit h) //Intersection Sphere - Rayon
-{
-	vec3	dist = r.pos - s.pos;
-	float	a = dot(r.dir, r.dir);
-	float	b = dot(r.dir, dist);
-	float	c = dot(dist, dist) - (s.radius * s.radius);
-	float	delta = b * b - a * c;
-	if (delta < EPSILONE)
-		return ;
-	float	t = (-sqrt(delta) -b) / a;
-	if (t < 0)
-		return ;
-	if (t < h.dist)
-	{
+	if (t < h.dist) {
 		h.dist = t;
 		h.pos = r.pos + r.dir * h.dist;
-		h.color = s.color;
-		h.norm = (h.pos - s.pos);
-		vec3	n = normalize(h.pos - s.pos);
-		h.uv = vec2(-(0.5 + (atan(n.z, n.x)) / (M_PI)),
-					(0.5 - asin(n.y)) / M_PI);
-	}
+		h.norm = faceforward(norm, norm, r.dir);
+    h.mat = mat;
+  }
+	if (data == 0)
+		return;
+	else if(h.pos.x > plus.x || h.pos.x < moin.x || h.pos.y > plus.y || h.pos.y < moin.y || h.pos.z > plus.z || h.pos.z < moin.z)
+		h = tmp;
 }
 
-void	iPlane(Plane p, Ray r, inout Hit h)//Intersection Plan - Rayon
+/* Découpe de sphère */
+void decoupe(vec3 centre, vec3 inter, Coupe c, Coupe c2, Material m, Ray r, inout Hit h, inout bool bo)
 {
-	float	t = (dot(p.norm, p.pos) - dot(p.norm, r.pos)) / dot(p.norm, r.dir);
-	if (t < EPSILONE)
-		return ;
-	if (t < h.dist)
-	{
-		h.dist = t;
-		h.pos = r.pos + r.dir * h.dist;
-		h.color = p.color;
-		h.norm = faceforward(p.norm, p.norm, r.dir);
-		vec3	u = vec3(p.norm.y, p.norm.z, -p.norm.x);
-		vec3	v = cross(u, p.norm);
-		h.uv = vec2(dot(h.pos, u), dot(h.pos, v));
+	c2.rot = normalize(c2.rot);
+	c.rot = normalize(c.rot);
+	vec3 pos = centre + c.rot * c.dist;
+	vec3 pos2 = centre + c2.rot * c2.dist;
+
+	float d = (c.rot.x * pos.x + c.rot.y * pos.y + c.rot.z * pos.z) * -1;
+	float d2 = (c2.rot.x * pos2.x + c2.rot.y * pos2.y + c2.rot.z * pos2.z) * -1;
+	bo = false;
+
+	if (c2.rot.x * inter.x + c2.rot.y * inter.y + c2.rot.z * inter.z + d2 > 0){
+		plane(c2.rot, pos2, 0, m, r, h);
+		bo = true;
 	}
+	if (c.rot.x * inter.x + c.rot.y * inter.y + c.rot.z * inter.z + d >= 0){
+		plane(c.rot, pos, 0, m, r, h);
+		bo = true;
+	}
+	return;
 }
 
-void	iCylinder(Cylinder cyl, Ray r, inout Hit h)
-{
-	vec3	d = r.pos - cyl.pos;
-	cyl.dir = normalize(cyl.dir);
-	float	a = dot(r.dir, r.dir) - pow(dot(r.dir, cyl.dir), 2);
-	float	b = 2 * (dot(r.dir, d) - dot(r.dir, cyl.dir) * dot(d, cyl.dir));
-	float	c = dot(d, d) - pow(dot(d, cyl.dir), 2) - pow(cyl.radius, 2);
-	float	g = b * b - 4 * a * c;
-	if (g < EPSILONE)
-		return ;
-	
-	float 	t1 = (-sqrt(g) - b) / (2 * a);
-	if (t1 < EPSILONE)
-		return ;
-	if (t1 > EPSILONE && t1 < h.dist)
-	{
-		h.dist = t1 - EPSILONE;
+void sphere (vec3 pos, float data, Material mat, Ray r, inout Hit h) {
+	vec3 d = r.pos - pos;
+
+	Coupe co;
+	co.rot = vec3(0,0,-1);
+	co.dist = 4;
+
+	Coupe co2;
+	co2.rot = vec3(0,1,0);
+	co2.dist = 1;
+
+	float a = dot (r.dir, r.dir);
+	float b = dot (r.dir, d);
+	float c = dot (d, d) - data * data;
+
+	float g = b*b - a*c;
+
+	if (g < EPSI)
+		return;
+
+	float t = (-sqrt (g) - b) / a;
+	vec3 inter = r.pos + r.dir * t;
+	bool bo;
+	decoupe(pos, inter, co, co2, mat, r, h, bo);
+
+	if (t < 0 || bo)
+		return;
+
+	if (t < h.dist) {
+		h.dist = t + EPSI;
 		h.pos = r.pos + r.dir * h.dist;
-		vec3	temp = cyl.dir * (dot(r.dir, cyl.dir) * h.dist + dot(r.pos - cyl.pos, cyl.dir));
-		vec3	tmp = h.pos - cyl.pos;
-		h.color = cyl.color;
+		h.norm = (h.pos - pos);
+		h.mat = mat;
+	}
+	return;
+}
+
+void cyl (vec3 v, vec3 dir, float data, Material mat, Ray r, inout Hit h) {
+	vec3 d = r.pos - v;
+
+	dir = normalize(dir);
+	float a = dot(r.dir,r.dir) - pow(dot(r.dir, dir), 2);
+	float b = 2 * (dot(r.dir, d) - dot(r.dir, dir) * dot(d, dir));
+	float c = dot(d, d) - pow(dot(d, dir), 2) - data * data;
+	float g = b*b - 4*a*c;
+
+	if (g < 0)
+		return;
+
+	float t1 = (-sqrt(g) - b) / (2*a);
+	//float t2 = (sqrt(g) - b) / (2*a);
+
+	if (t1 < 0)
+		return ;
+
+	if (t1 < h.dist){
+		h.dist = t1 - EPSI;
+		h.pos = r.pos + r.dir * h.dist;
+		vec3 temp = dir * (dot(r.dir, dir) * h.dist + dot(r.pos - v, dir));
+		vec3 tmp = h.pos - v;
 		h.norm = tmp - temp;
-		vec3	d = h.pos - (cyl.pos * r.dir);
-		h.uv = vec2(-(0.5 + (atan(d.z, d.x) / (M_PI * 0.25))), -((d.y / M_PI) - floor(d.y / M_PI)));
+		h.mat = mat;
 	}
+	/*else if (t2 >= 0 && t2 < h.dist){
+	  h.dist = t2;
+	  h.pos = r.pos + r.dir * t2;
+	  vec3 temp = rot * (dot(r.dir, rot) * h.dist + dot(r.pos - v, rot));
+	  vec3 tmp = h.pos - v;
+	  h.color = color;
+	  h.norm = temp - tmp;
+	  }*/
 }
 
-void	iCone(Cone co, Ray r, inout Hit h)
+Hit		scene(Ray r)
 {
-	vec3	d = r.pos - co.pos;
-	co.dir = normalize(co.dir);
-	float	a = dot(r.dir, r.dir) - (1 + pow(tan(co.angle), 2)) * pow(dot(r.dir, co.dir), 2);
-	float	b = 2 * (dot(r.dir, d) - (1 + pow(tan(co.angle), 2)) * dot(r.dir, co.dir) * dot(d, co.dir));
-	float	c = dot(d, d) - (1 + pow(tan(co.angle), 2)) * pow(dot(d, co.dir), 2);
-	float	g = b * b - 4 * a * c;
-	if (g <= 0)
-		return ;
+	int i = -1;
+	Hit		hit;
+	hit.dist = 1e20;
+	hit.mat.texture = vec4(0,0,0,0);
+  sphere(vec3(6, 14, -1), 5, Material(vec4(1,0,0,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,1,1,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,0,0,0)), r, hit );
 
-	float t1 = (-sqrt(g) - b) / (2 * a);
-	if (t1 < EPSILONE)
-		return ;
-	if (t1 < h.dist)
-	{
-		h.dist = t1;
-		h.pos = r.pos + r.dir * h.dist;
-		vec3	temp = (co.dir * (dot(r.dir, co.dir) * h.dist + dot(r.pos - co.pos, co.dir))) * (1 + pow(tan(co.angle), 2));
-		vec3	tmp = h.pos - co.pos;
-		h.color = co.color;
-		h.norm = tmp - temp;
-		vec3	d = normalize(h.pos - co.pos);
-		h.uv = vec2(-(0.5 + (atan(h.norm.z, h.norm.x) / (M_PI * 2))), (h.norm.y /  M_PI) - floor(h.norm.y / M_PI));
-	}
+	cyl(vec3(0, 0, 0), vec3(0,0,1), 0.2, Material(vec4(0,0,1,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(1,1,1,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,0,0,0)), r, hit );
+	cyl(vec3(0, 0, 0), vec3(1,0,0), 0.2, Material(vec4(1,0,0,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(1,1,1,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,0,0,0)), r, hit );
+	cyl(vec3(0, 0, 0), vec3(0,1,0), 0.2, Material(vec4(0,1,0,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(1,1,1,1), vec4(0,0,0,0), vec4(0,0,0,0), vec4(0,0,0,0)), r, hit );
+
+  return hit;
 }
 
-Hit		Scene(Ray r)
+float		limit(float value, float min, float max)
 {
-	Hit	h;
-	h.dist = 1e20;
-	h.color = vec3(0, 0, 0);
-
-	Sphere		s = Sphere(vec3(20, 3, 0), vec3(0.3,0.4, 0.8), 5);
-	Cylinder	cyl = Cylinder(vec3(0, 0, 0), vec3(0, 1, 0), vec3(0.5, 0.8, 0.2), 5);
-	Plane		p = Plane(vec3(0, -5, 0), vec3(0, 1, 0), vec3(0.8, 0.3, 0.8));
-	Cone		co = Cone(vec3(-20, 3, 0), vec3(0, 1, 0), vec3(0.8, 0.8, 0.4), M_PI * 15/180);
-
-	iSphere(s, r, h);
-	iCylinder(cyl, r, h);
-	iPlane(p, r, h);
-	iCone(co, r, h);
-	return (h);
+	return (value < min ? min : (value > max ? max : value));
 }
 
-float	light(Light l, Ray r, Hit h)
+bool			shadows(vec3 pos, vec3 d, Hit h)
 {
-	vec3	v1 = l.pos - h.pos;
-	vec3	v3 = v1 * v1;
-	vec3	dist = normalize(v1);
-	float	lambert = dot(dist, h.norm);
+	Ray		shadow;
+	Hit		shad;
 
+	shadow.dir = d;
+	shadow.pos = pos + h.norm * EPSI;
+	shad = scene(shadow);
+	if (shad.dist < h.dist)
+		return (true);
+	return (false);
+}
+
+/* Définition de l'effet de la lumière sur les objets présents */
+vec3		light(vec3 pos, Ray r, Hit h)
+{
+	vec3 v1 = pos - h.pos;
+	vec3 v3 = v1 * v1;
+	vec3 d = normalize(v1);
+	vec3 color;
+
+  color = vec3(AMBIENT * vec3(h.mat.texture.xyz));
 	h.dist = sqrt(v3.x + v3.y + v3.z);
-	return (limit(lambert, 0.15, 1));
-}
-
-vec4	raycast(vec3 ro, vec3 rd)
-{
-	vec4	color = vec4(0, 0, 0, 1);
-	Ray		r;
-	Hit		h;
-	float	l1;
-	Light	l = Light(vec3(0, 0, 0), vec3(0, 18, -10));
-
-	r.dir = rd;
-	r.pos = ro;
-	h = Scene(r);
-	l1 = light(l, r, h);
-	if (color.xyz != vec3(0))
-	{
-		color.xyz = h.color * l1;
+	if (h.dist > 1e20)
 		return (color);
-	}
-	else
-	{
-		vec4 ctexture = texture(iChannel0, h.uv);
-		return (ctexture);
-	}
+	if (shadows(h.pos, d, h))
+		return (color);
+	color += (limit(dot(h.norm, d), 0.0, 1.0)) * vec3(h.mat.texture.xyz);
+  return (color);
 }
 
-void	mainImage(vec2 FragCoord)
+/* Définition de la light */
+vec3		calc_light(vec3 pos, Ray ref, Hit h)
 {
-	vec2	uv = (FragCoord / iResolution) * 2 - 1;
-	vec3	cameraPos = iMoveAmount.xyz + vec3(0, 5, -17);
-	vec3	cameraDir = vec3(0, 0, 0);
+	Hit	h2 = h;
+	vec3 lambert;
+	vec3 reflect = vec3(0,0,0);
+	vec3 ambient = vec3(h.mat.texture.xyz) * AMBIENT;
+	int		i = 0;
+	float on_off = 1;
+	lambert = light(pos, ref, h);
+	/*while (++i < 5)
+	{
+	h = h2;
+	ref.dir = h.norm;
+	ref.pos = h.pos;
+	h2 = scene(ref);
+	on_off = on_off * h.mat.reflection.x;
+	reflect += light(pos, ref, h2) * on_off;
+	}*/
+	return (lambert + reflect);
+}
+
+
+/* Création d'un rayon */
+vec3	raytrace(vec3 ro, vec3 rd)
+{
+	Ray			r;
+	Hit			h;
+	vec3		pos_lum = vec3(15,15,-25);
+	vec3		color = vec3(0,0,0);
+
+  r.dir = rd;
+	r.pos = ro;
+	h = scene(r);
+	color = calc_light(pos_lum, r, h);
+  return (color / 2);
+}
+
+void		mainImage(vec2 coord)
+{
+	vec2	uv = (coord / iResolution) * 2 - 1;
+	vec3	cameraPos = iMoveAmount.xyz + vec3(0, 0, -10);
+	vec3	cameraDir = vec3(0,0,0);
+	vec3	col;
 
 	uv.x *= iResolution.x / iResolution.y;
-
 	float	fov = 1.5;
-	vec3	forward = normalize(iForward);
-	vec3	right = normalize(cross(forward, vec3(0, 1, 0)));
-	vec3	up = normalize(cross(right, forward));
-	vec3	rd = normalize(uv.x * right + uv.y * up + fov * forward);
-	fragColor = vec4(raycast(cameraPos,rd));
+	vec3	forw = normalize(iForward);
+	vec3	right = normalize(cross(forw, vec3(0, 1, 0)));
+	vec3	up = normalize(cross(right, forw));
+	vec3	rd = normalize(uv.x * right + uv.y * up + fov * forw);
+	fragColor = vec4(raytrace(cameraPos, rd), 1);
 }
